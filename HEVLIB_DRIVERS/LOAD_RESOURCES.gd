f6135a7f374490d7d8462e04ec11307962c1665b
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-06-10 22:07:14 -0700

    made necessary changes to convert to a modlet

---
+++ HEVLIB_DRIVERS/LOAD_RESOURCES.gd
@@ -0,0 +1,6 @@
+extends Node
+
+const LOAD_RESOURCES = {
+	"TitleScreen.tscn":{"load_type":"scene","original_path":"TitleScreen.tscn","onready":true},
+	"PauseLayer.gd":{"load_type":"script","onready":false},
+}
